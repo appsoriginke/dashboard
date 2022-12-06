ab62e81f973b162fc6682b9da46a1dc36d776a69
philipnjuguna66 <philipnjuguna66@gmail.com>
2022-12-06 12:12:08 +0300

    add theme color

--- resources/views/layouts/_partials/_nav.blade.php
+++ resources/views/layouts/_partials/_nav.blade.php
@@ -3,7 +3,7 @@
     @foreach(config('dashboard.navigations') as $name => $navigation)
         @can($navigation['canAccess'])
         <a href="{{ route($navigation['url'] ) }}"
-           class="text-green-100 hover:text-white hover:bg-green-500 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
+           class="text-{{ config("dashboard.primary-theme-color") }}-100 hover:text-white hover: bg-{{ config("dashboard.primary-theme-color") }}-500 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md"
            aria-current="page">
             <x-dynamic-component :component="$navigation['icon']" class="h-6 w-6 mr-1" />
 
